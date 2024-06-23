a895096151b7bb8b81395348d0616b3afba46556
Josh Smith <cmyuiosu@gmail.com>
2024-06-22 20:35:33 -0400

    Remove unused indices on large user_profile_history table (#10)

---
+++ migrations/0007_remove_unused_profile_history_indices.up.sql
@@ -0,0 +1,4 @@
+alter table user_profile_history drop index rank;
+alter table user_profile_history drop index rank_2;
+alter table user_profile_history drop index captured_at;
+alter table user_profile_history drop index user_id_2;
