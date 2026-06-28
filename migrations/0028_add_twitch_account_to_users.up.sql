a8621e031c4c847767cb2e18f1620d18f3fb9d98
Josh Smith <cmyuiosu@gmail.com>
2026-06-27 21:30:36 -0400

    Add Twitch account fields to users (#34)
    * Add Twitch account fields to users
    
    * Remove cached Twitch display name

---
+++ migrations/0028_add_twitch_account_to_users.up.sql
@@ -0,0 +1,5 @@
+alter table users
+  add column twitch_account_id varchar(32) null default null,
+  add column twitch_username varchar(25) null default null,
+  add unique key users_twitch_account_id_unique (twitch_account_id),
+  add key users_twitch_username_idx (twitch_username);
